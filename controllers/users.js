208fd103bcfb39d610f97b231f9f41cb9b48b58f
salasousi <sarahalasousi@gmail.com>
2022-12-21 15:50:16 -0500

    first commit

---
+++ controllers/users.js
@@ -0,0 +1,36 @@
+
+// Dependencies
+const bcrypt = require('bcrypt');
+const express = require('express');
+const userRouter = express.Router();
+const User = require('../models/user.js');
+
+//I
+
+// New (registration page)
+userRouter.get('/new', (req, res) => {
+	res.render('users/new.ejs', {
+		currentUser: req.session.currentUser
+	})
+})
+
+//D
+
+//U
+
+// Create (registration route)
+userRouter.post('/', (req, res) => {
+    //overwrite the user password with the hashed password, then pass that in to our database
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+
+    User.create(req.body, (error, createdUser) => {
+        res.redirect('/')
+    })
+})
+
+//E
+
+//S
+
+// Export User Router
+module.exports = userRouter;
